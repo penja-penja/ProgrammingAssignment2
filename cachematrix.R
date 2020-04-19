edcfb7be154e360c39ec93dadac0ea99cc50741f
Ashley Su <penja-penja@users.noreply.github.com>
2020-04-19 15:04:41 +1000

    [coursera data science week3] - Assignment on Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Returns a list that contains the 2 pairs of getter and setter methods include:
+## set the value of the vector
+## get the value of the vector
+## set the inverse value
+## get the inverse value
 makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  
+  # Accessors
+  get <- function() x
+  getInverse <- function() inv
+  
+  # Mutators
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  setInverse <- function(newInv) inv <<- newInv
 
+  # Return the functions
+  list(set = set, get = get, 
+       setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Calculate the inverse of the input value x and caches it
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  
+  # Calculated inverse value if it has not been calculated
+  if(is.null(inv)) {
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setInverse(inv)
+  }
+  
+  inv
 }
